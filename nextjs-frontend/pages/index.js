8f56e9297d7b466822cc8cea55af4aa202bb84a3
Andrew-MartinezH <72889964+Andrew-MartinezH@users.noreply.github.com>
2020-10-28 17:10:33 -0300

    Creacion de APP con React y Next.js

---
+++ nextjs-frontend/pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import styles from '../styles/form_sim_loan.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+
+
+    </div>
+  )
+}
